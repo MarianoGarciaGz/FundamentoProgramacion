6aab6b7604b95b211a934072ccb4c2c0ca7261d2
MarianoGarciaGz <marianogarciagz@gmail.com>
2020-11-12 21:17:12 -0600

    FEAT: Se agrego el archivo de Condiciones

---
+++ Basicos/Condiciones.py
@@ -0,0 +1,12 @@
+#Si es par, non o igual a cero
+
+num = int(input("Ingresa un número: "))
+
+if (num==0):
+    print("Tu número es cero")
+
+elif (num%2==0):
+    print("Tu número es par")
+
+else:
+    print("Tu número es non") 
