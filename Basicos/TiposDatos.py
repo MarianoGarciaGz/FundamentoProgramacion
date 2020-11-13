ece051f6c98d7c7ee5cb0e3c720ee13c6f71050a
MarianoGarciaGz <marianogarciagz@gmail.com>
2020-11-12 20:55:06 -0600

    FEAT: Se agrego el archivo TipoDatos

---
+++ Basicos/TiposDatos.py
@@ -0,0 +1,7 @@
+#Todos los tipos de datos en Python
+
+entero=10 #Tipo de dato Int
+flotante=10.1 #Tipo de dato Flotante
+texto="Hola" #Tipo de dato String
+bandera=True #Tipo de dato booleano (Puede ser verdadero o falso)
+lista=[1,2,3,4,5] #Tipo de dato Lista *Arreglo (Puede contener n cantidad de valores)
